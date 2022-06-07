304183edf9864ff3f8f73d1df227e9b069bcea4f
Kiko <kristiqnenchevv@gmail.com>
2022-06-07 14:48:20 +0300

    Create Tag Dto

---
+++ Server/Infrastructure/Models/Blog/TagDto.cs
@@ -0,0 +1,20 @@
+﻿namespace Models.Blog
+{
+    using Models;
+
+    using Domain.Entities.Blog;
+
+    public class TagDto : BaseDto<TagDto, Tag>
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        public List<BlogPostDto> BlogPosts { get; set; } = new();
+
+        public override void CustomizeMapping(Mapster.TypeAdapterConfig config)
+        {
+            config.NewConfig<Tag, TagDto>()
+                .Map(dest => dest.BlogPosts, src => src.BlogPosts);
+        }
+    }
+}
