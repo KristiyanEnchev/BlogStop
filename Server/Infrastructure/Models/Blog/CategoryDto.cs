1478ae2701d2cf393926838a2f3d1e3116fd8e7d
Kiko <kristiqnenchevv@gmail.com>
2022-06-07 13:29:13 +0300

    Create category dto

---
+++ Server/Infrastructure/Models/Blog/CategoryDto.cs
@@ -0,0 +1,19 @@
+﻿namespace Models.Blog
+{
+    using Domain.Entities.Blog;
+
+    public class CategoryDto : BaseDto<CategoryDto, Category>
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        public string Description { get; set; }
+        public List<BlogPostDto> BlogPosts { get; set; } = new();
+
+        public override void CustomizeMapping(Mapster.TypeAdapterConfig config)
+        {
+            config.NewConfig<Category, CategoryDto>()
+                .Map(dest => dest.BlogPosts, src => src.BlogPosts);
+        }
+    }
+}
