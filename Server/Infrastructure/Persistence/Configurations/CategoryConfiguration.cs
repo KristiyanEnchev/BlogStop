0a210f72999a37edd6c74cea91ed654ac58e7ce3
Kiko <kristiqnenchevv@gmail.com>
2022-06-09 15:22:18 +0300

    Create category configuration

---
+++ Server/Infrastructure/Persistence/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,20 @@
+﻿namespace Persistence.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    using Domain.Entities.Blog;
+
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Categories", "Blog");
+
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Name).HasMaxLength(100).IsRequired();
+            builder.Property(c => c.Slug).HasMaxLength(100).IsRequired();
+            builder.Property(c => c.Description).HasMaxLength(500);
+        }
+    }
+}
