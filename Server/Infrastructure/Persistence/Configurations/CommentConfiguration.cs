ae276d7cd86b524f7b104e9a5d13d6bfe7b5003b
Kiko <kristiqnenchevv@gmail.com>
2022-06-09 12:44:54 +0300

    Create comment configuration

---
+++ Server/Infrastructure/Persistence/Configurations/CommentConfiguration.cs
@@ -0,0 +1,31 @@
+﻿namespace Persistence.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    using Domain.Entities.Blog;
+
+    public class CommentConfiguration : IEntityTypeConfiguration<Comment>
+    {
+        public void Configure(EntityTypeBuilder<Comment> builder)
+        {
+            builder.ToTable("Comments", "Blog");
+
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Content).IsRequired();
+            builder.Property(c => c.NumberOfLikes).HasDefaultValue(0);
+
+            builder
+                .HasOne(c => c.BlogPost)
+                .WithMany(bp => bp.Comments)
+                .HasForeignKey(c => c.BlogPostId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder
+                .HasOne(c => c.Author)
+                .WithMany()
+                .HasForeignKey(c => c.AuthorId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
