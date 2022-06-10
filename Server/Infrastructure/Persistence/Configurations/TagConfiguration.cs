9641aa317626cc08af094ece141401f82ae0f932
Kiko <kristiqnenchevv@gmail.com>
2022-06-10 13:25:49 +0300

    Create Tag configuration

---
+++ Server/Infrastructure/Persistence/Configurations/TagConfiguration.cs
@@ -0,0 +1,19 @@
+﻿namespace Persistence.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    using Domain.Entities.Blog;
+
+    public class TagConfiguration : IEntityTypeConfiguration<Tag>
+    {
+        public void Configure(EntityTypeBuilder<Tag> builder)
+        {
+            builder.ToTable("Tags", "Blog");
+
+            builder.HasKey(t => t.Id);
+            builder.Property(t => t.Name).HasMaxLength(100).IsRequired();
+            builder.Property(t => t.Slug).HasMaxLength(100).IsRequired();
+        }
+    }
+}
