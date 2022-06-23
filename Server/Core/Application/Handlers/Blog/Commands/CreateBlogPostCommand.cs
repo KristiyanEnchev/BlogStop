bd9a721f78df3bd01bf1528e0137bf87dbe8d45f
Kiko <kristiqnenchevv@gmail.com>
2022-06-23 12:35:00 +0300

    Start with the commands , create CreateBlogPostCommand

---
+++ Server/Core/Application/Handlers/Blog/Commands/CreateBlogPostCommand.cs
@@ -0,0 +1,41 @@
+﻿namespace Application.Handlers.Blog.Commands
+{
+    using Microsoft.Extensions.Logging;
+
+    using MediatR;
+
+    using Models.Blog;
+
+    using Shared;
+
+    using Application.Interfaces;
+
+    public record CreateBlogPostCommand(BlogPostRequest Request) : IRequest<Result<BlogPostDto>>
+    {
+        public class Handler : IRequestHandler<CreateBlogPostCommand, Result<BlogPostDto>>
+        {
+            private readonly IBlogService _blogService;
+            private readonly ILogger<Handler> _logger;
+
+            public Handler(IBlogService blogService, ILogger<Handler> logger)
+            {
+                _blogService = blogService;
+                _logger = logger;
+            }
+
+            public async Task<Result<BlogPostDto>> Handle(CreateBlogPostCommand request, CancellationToken cancellationToken)
+            {
+                try
+                {
+                    var blogPost = await _blogService.CreateBlogPostAsync(request.Request);
+                    return Result<BlogPostDto>.SuccessResult(blogPost);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Error creating blog post");
+                    throw;
+                }
+            }
+        }
+    }
+}
