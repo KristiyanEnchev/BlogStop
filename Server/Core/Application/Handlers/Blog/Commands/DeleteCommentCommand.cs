76a893b974bc6673c7a98533720204a1e8dd893a
Kiko <kristiqnenchevv@gmail.com>
2022-06-28 12:58:01 +0300

    Create DeleteCommentCommand

---
+++ Server/Core/Application/Handlers/Blog/Commands/DeleteCommentCommand.cs
@@ -0,0 +1,39 @@
+﻿namespace Application.Handlers.Blog.Commands
+{
+    using Microsoft.Extensions.Logging;
+
+    using MediatR;
+
+    using Shared;
+
+    using Application.Interfaces;
+
+    public record DeleteCommentCommand(string CommentId) : IRequest<Result<bool>>
+    {
+        public class Handler : IRequestHandler<DeleteCommentCommand, Result<bool>>
+        {
+            private readonly IBlogService _blogService;
+            private readonly ILogger<Handler> _logger;
+
+            public Handler(IBlogService blogService, ILogger<Handler> logger)
+            {
+                _blogService = blogService;
+                _logger = logger;
+            }
+
+            public async Task<Result<bool>> Handle(DeleteCommentCommand request, CancellationToken cancellationToken)
+            {
+                try
+                {
+                    var success = await _blogService.DeleteCommentAsync(request.CommentId);
+                    return Result<bool>.SuccessResult(success);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Error deleting comment {CommentId}", request.CommentId);
+                    throw;
+                }
+            }
+        }
+    }
+}
