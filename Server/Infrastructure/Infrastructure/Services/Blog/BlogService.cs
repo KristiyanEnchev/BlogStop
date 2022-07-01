e35ab1cd42997d0cb5896b6077270b34a2bdf5d5
Kiko <kristiqnenchevv@gmail.com>
2022-07-01 14:21:49 +0300

    Create Crud for categories

--- Server/Infrastructure/Infrastructure/Services/Blog/BlogService.cs
+++ Server/Infrastructure/Infrastructure/Services/Blog/BlogService.cs
@@ -310,5 +310,48 @@
                 .ProjectToType<CategoryDto>()
                 .FirstOrDefaultAsync();
         }
+
+        public async Task<CategoryDto> CreateCategoryAsync(string name, string description)
+        {
+            var newCategory = new Category
+            {
+                Name = name,
+                Slug = name.ToLower().Replace(" ", "-"),
+                Description = description
+            };
+
+            await _categoryRepository.AddAsync(newCategory);
+            await _categoryRepository.SaveChangesAsync();
+
+            return newCategory.Adapt<CategoryDto>();
+        }
+
+        public async Task<bool> UpdateCategoryAsync(string categoryId, string name, string description)
+        {
+            var category = await _categoryRepository.AsTracking()
+                .FirstOrDefaultAsync(c => c.Id == categoryId);
+
+            if (category == null) return false;
+
+            category.Name = name;
+            category.Slug = name.ToLower().Replace(" ", "-");
+            category.Description = description;
+
+            await _categoryRepository.UpdateAsync(category);
+            await _categoryRepository.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> DeleteCategoryAsync(string categoryId)
+        {
+            var category = await _categoryRepository.AsTracking()
+                .FirstOrDefaultAsync(c => c.Id == categoryId);
+
+            if (category == null) return false;
+
+            await _categoryRepository.DeleteAsync(category);
+            await _categoryRepository.SaveChangesAsync();
+            return true;
+        }
     }
 }
