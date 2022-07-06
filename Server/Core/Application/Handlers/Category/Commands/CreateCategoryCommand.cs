1ca9c10ed6e624c63ea1c2b928975e2299d4fd4b
Kiko <kristiqnenchevv@gmail.com>
2022-07-06 12:53:14 +0300

    Create CreateCategoryCommand

---
+++ Server/Core/Application/Handlers/Category/Commands/CreateCategoryCommand.cs
@@ -0,0 +1,40 @@
+﻿namespace Application.Handlers.Categories.Commands
+{
+    using Microsoft.Extensions.Logging;
+
+    using MediatR;
+
+    using Shared;
+
+    using Models.Blog;
+
+    using Application.Interfaces;
+
+    public record CreateCategoryCommand(string Name, string Description) : IRequest<Result<CategoryDto>>;
+
+    public class CreateCategoryCommandHandler : IRequestHandler<CreateCategoryCommand, Result<CategoryDto>>
+    {
+        private readonly ICategoryService _categoryService;
+        private readonly ILogger<CreateCategoryCommandHandler> _logger;
+
+        public CreateCategoryCommandHandler(ICategoryService categoryService, ILogger<CreateCategoryCommandHandler> logger)
+        {
+            _categoryService = categoryService;
+            _logger = logger;
+        }
+
+        public async Task<Result<CategoryDto>> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var category = await _categoryService.CreateCategoryAsync(request.Name, request.Description);
+                return Result<CategoryDto>.SuccessResult(category);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error creating category");
+                return Result<CategoryDto>.Failure(new List<string> { ex.Message });
+            }
+        }
+    }
+}
