34a205f1ac51a49da80dc99b3e49aac740007882
Kiko <kristiqnenchevv@gmail.com>
2022-07-11 13:58:22 +0300

    Create CreateTagCommand

---
+++ Server/Core/Application/Handlers/Tag/Commnads/CreateTagCommand.cs
@@ -0,0 +1,41 @@
+﻿namespace Application.Handlers.Tag.Commnads
+{
+    using Microsoft.Extensions.Logging;
+
+    using Application.Interfaces;
+
+    using MediatR;
+
+    using Models.Blog;
+
+    using Shared;
+
+    public record CreateTagCommand(string Name) : IRequest<Result<TagDto>>
+    {
+        public class CreateTagCommandHandler : IRequestHandler<CreateTagCommand, Result<TagDto>>
+        {
+            private readonly ITagService _tagService;
+            private readonly ILogger<CreateTagCommandHandler> _logger;
+
+            public CreateTagCommandHandler(ITagService tagService, ILogger<CreateTagCommandHandler> logger)
+            {
+                _tagService = tagService;
+                _logger = logger;
+            }
+
+            public async Task<Result<TagDto>> Handle(CreateTagCommand request, CancellationToken cancellationToken)
+            {
+                try
+                {
+                    var newTag = await _tagService.CreateTagAsync(request.Name);
+                    return Result<TagDto>.SuccessResult(newTag);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Error creating tag {Name}", request.Name);
+                    return Result<TagDto>.Failure(new List<string> { "Failed to create tag." });
+                }
+            }
+        }
+    }
+}
