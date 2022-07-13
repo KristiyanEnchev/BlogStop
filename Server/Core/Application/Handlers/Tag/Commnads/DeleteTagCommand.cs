d523e1c9333ee15dad38cb8b44cbcc3d7180aed8
Kiko <kristiqnenchevv@gmail.com>
2022-07-13 12:03:53 +0300

    Create DeleteTagCommand

---
+++ Server/Core/Application/Handlers/Tag/Commnads/DeleteTagCommand.cs
@@ -0,0 +1,41 @@
+﻿namespace Application.Handlers.Tag.Commnads
+{
+    using Microsoft.Extensions.Logging;
+
+    using Application.Interfaces;
+
+    using MediatR;
+
+    using Shared;
+
+    public record DeleteTagCommand(string TagId) : IRequest<Result<bool>>
+    {
+        public class DeleteTagCommandHandler : IRequestHandler<DeleteTagCommand, Result<bool>>
+        {
+            private readonly ITagService _tagService;
+            private readonly ILogger<DeleteTagCommandHandler> _logger;
+
+            public DeleteTagCommandHandler(ITagService tagService, ILogger<DeleteTagCommandHandler> logger)
+            {
+                _tagService = tagService;
+                _logger = logger;
+            }
+
+            public async Task<Result<bool>> Handle(DeleteTagCommand request, CancellationToken cancellationToken)
+            {
+                try
+                {
+                    var result = await _tagService.DeleteTagAsync(request.TagId);
+                    return result
+                        ? Result<bool>.SuccessResult(true)
+                        : Result<bool>.Failure(new List<string> { "Tag deletion failed." });
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Error deleting tag {TagId}", request.TagId);
+                    return Result<bool>.Failure(new List<string> { "Failed to delete tag." });
+                }
+            }
+        }
+    }
+}
