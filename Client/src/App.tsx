8948c3880f05463bb05c0d8f5cfa608ff9d47224
Kiko <kristiqnenchevv@gmail.com>
2022-07-19 12:34:03 +0300

    Set up app with store and router

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,13 +1,25 @@
+import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
-function App() {
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './store';
+import AppRoutes from './routes';
 
+function App() {
   return (
-    <Toaster
-      position="bottom-right"
-      toastOptions={{
-        duration: 3000,
-      }}
-    />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <AppRoutes />
+          <Toaster
+            position="bottom-right"
+            toastOptions={{
+              duration: 3000,
+            }}
+          />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   );
 }
 
