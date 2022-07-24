af809c82a086e9861745b95199779f1fd9b5bc62
Kiko <kristiqnenchevv@gmail.com>
2022-07-24 12:07:14 +0300

    Create guest guard

---
+++ Client/src/routes/GuestRoute.tsx
@@ -0,0 +1,20 @@
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAppSelector } from '@/store/hooks';
+import { selectIsAuthenticated, selectAuthLoading } from '@/services/auth/authSlice';
+
+const GuestRoute = () => {
+    const isAuthenticated = useAppSelector(selectIsAuthenticated);
+    const isLoading = useAppSelector(selectAuthLoading);
+
+    if (isLoading) {
+        return <div className="flex justify-center items-center h-screen">Loading...</div>;
+    }
+
+    if (isAuthenticated) {
+        return <Navigate to="/" replace />;
+    }
+
+    return <Outlet />;
+};
+
+export default GuestRoute;
