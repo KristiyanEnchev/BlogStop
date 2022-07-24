c6cf3044483c123a1d1b7ae1773f272b124b3508
Kiko <kristiqnenchevv@gmail.com>
2022-07-24 13:37:59 +0300

    Create protected routes gueard

---
+++ Client/src/routes/ProtectedRoute.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAppSelector } from '../store/hooks';
+import { selectIsAuthenticated, selectAuthLoading } from '@/services/auth/authSlice';
+
+interface ProtectedRouteProps {
+    requiredRoles?: string[];
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ requiredRoles }) => {
+    const isAuthenticated = useAppSelector(selectIsAuthenticated);
+    const isLoading = useAppSelector(selectAuthLoading);
+    const user = useAppSelector(state => state.auth.user);
+
+    if (isLoading) {
+        return <div className="flex justify-center items-center h-screen">Loading...</div>;
+    }
+
+    if (!isAuthenticated) {
+        return <Navigate to="/login" replace />;
+    }
+
+    if (requiredRoles && requiredRoles.length > 0) {
+        const hasRequiredRole = user?.roles.some(role => requiredRoles.includes(role));
+
+        if (!hasRequiredRole) {
+            return <Navigate to="/" replace />;
+        }
+    }
+
+    return <Outlet />;
+};
+
+export default ProtectedRoute;
