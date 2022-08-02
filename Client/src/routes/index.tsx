4f8e49d21a8ecf4587bd234656de1312d00a7790
Kiko <kristiqnenchevv@gmail.com>
2022-08-02 12:32:45 +0300

    Add all the paths to the router

--- Client/src/routes/index.tsx
+++ Client/src/routes/index.tsx
@@ -1,11 +1,26 @@
 import { Route, Routes } from "react-router-dom";
 import { MainLayout } from "@/components/layout/MainLayout";
+import { lazy } from "react";
+import GuestRoute from "./GuestRoute";
+import PageWrapper from "@/components/common/PageWrapper";
+import ProtectedRoute from "./ProtectedRoute";
+
+const NotFound = lazy(() => import("@/pages/NotFound"));
+const Login = lazy(() => import("@/pages/Login"));
+const Register = lazy(() => import("@/pages/Register"));
 
 export default function AppRoutes() {
     return (
         <Routes>
             <Route path="/" element={<MainLayout />}>
+                <Route element={<GuestRoute />}>
+                    <Route path="/login" element={<PageWrapper component={Login} />} />
+                    <Route path="/register" element={<PageWrapper component={Register} />} />
+                </Route>
+                <Route element={<ProtectedRoute />}>
+                </Route>
             </Route>
+            <Route path="*" element={<PageWrapper component={NotFound} />} />
         </Routes>
     );
 }
