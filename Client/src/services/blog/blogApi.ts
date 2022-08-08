4a5e12a0076c0e72a24ac11129cc1544a4c4f5c0
Kiko <kristiqnenchevv@gmail.com>
2022-08-08 10:36:27 +0300

    Add getBlogPosts endpoint

--- Client/src/services/blog/blogApi.ts
+++ Client/src/services/blog/blogApi.ts
@@ -1,12 +1,24 @@
 import { createApi } from '@reduxjs/toolkit/query/react';
-import { baseQuery } from '../auth/baseQueryConfig';
+import { baseQueryWithReauth } from '../auth/baseQueryWithReauth';
 import { BlogPost, BlogQueryParams, PaginatedResult } from '@/types/blog';
 
 export const blogApi = createApi({
   reducerPath: 'blogApi',
-  baseQuery,
+  baseQuery: baseQueryWithReauth,
   tagTypes: ['Blog', 'Comment'],
   endpoints: (builder) => ({
-    // Endpoints will be added in subsequent commits
+    getBlogPosts: builder.query<PaginatedResult<BlogPost>, BlogQueryParams>({
+      query: (params) => ({
+        url: 'blog',
+        params,
+      }),
+      providesTags: (result) => 
+        result
+          ? [
+              ...result.items.map(({ id }) => ({ type: 'Blog' as const, id })),
+              { type: 'Blog', id: 'LIST' },
+            ]
+          : [{ type: 'Blog', id: 'LIST' }],
+    }),
   }),
 });
